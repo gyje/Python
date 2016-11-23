015a5b6df2c6f67fa4093521fbddfa8fadafdd0c
YingJie <gyj37191@gmail.com>
2016-11-23 18:53:58 +0800

    del file

--- 5.py
+++ 5.py
@@ -1,8 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 #print (requests.get("https://www.tuicool.com"))#没有加入header和cookie所以被反爬虫检测,失败
 headers={"UserAgent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 url="http://www.tuicool.com"
 r=requests.get(url)
-print (r.url)
-print (r.text)
-
+#print (r.url)
+#print (r.text)
+soup=requests(r.title)
+print (soup.title)
