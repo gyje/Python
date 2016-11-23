4dcfdc152e34fc6d8f1523d1916af573c5a63352
YingJie <gyj37191@gmail.com>
2016-11-23 23:35:39 +0800

    add 5.py use BS4,but

--- 5.py
+++ 5.py
@@ -1,10 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-#print (requests.get("https://www.tuicool.com"))#没有加入header和cookie所以被反爬虫检测,失败
 headers={"UserAgent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-url="http://www.tuicool.com"
-r=requests.get(url)
-#print (r.url)
-#print (r.text)
-soup=requests(r.title)
-print (soup.title)
+#cookies=dict(cookies=)
+#url="http://jandan.net/ooxx/page-2008"
+url="https://zhuanlan.zhihu.com/p/20858208"
+r=requests.get(url,headers=headers)
+print (r.url)
+connect=r.text
+soup=BeautifulSoup(connect,'lxml')
+url = soup.select('p > img')
+print (connect)
+print (r.encoding)
+print (r.json)
+"""
+看了网上的一些文档主要来源是推酷和csdn,知乎,还有一些blog,对Beautifulsoup的评价都是不如lxml,而lxml则不如pyquery,所以决定看一看lxml,BeautifulSoup终结.
